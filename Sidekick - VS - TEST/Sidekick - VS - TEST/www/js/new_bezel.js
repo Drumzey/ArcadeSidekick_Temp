46676f0eae7791645e64cd44e53f57fabe1df911
Richard Rumsey <richard.rumsey@sage.com>
2019-02-16 21:24:19 +0000

    Added more bezels

--- Sidekick - VS - TEST/Sidekick - VS - TEST/www/js/new_bezel.js
+++ Sidekick - VS - TEST/Sidekick - VS - TEST/www/js/new_bezel.js
@@ -4,6 +4,121 @@ var defaultBezelImage = 'default.png';
 
 var bezelDimensions = [];
 
+//Beat em ups
+var baddudes = new Bezel();
+baddudes.id = 'bad dudes vs dragon ninja';
+baddudes.topOffset = 25;
+baddudes.leftOffset = 15;
+baddudes.screenWidth = 70;
+baddudes.screenHeight = 50;
+baddudes.image = 'bad dudes vs dragon ninja.png';
+bezelDimensions['bad_dudes_vs_dragon_ninja'] = baddudes;
+
+var doubledragon = new Bezel();
+doubledragon.id = 'double dragon';
+doubledragon.topOffset = 22;
+doubledragon.leftOffset = 13;
+doubledragon.screenWidth = 74;
+doubledragon.screenHeight = 56;
+doubledragon.image = 'double dragon.png';
+bezelDimensions['double_dragon'] = doubledragon;
+
+var finalfight = new Bezel();
+finalfight.id = 'final fight';
+finalfight.topOffset = 24;
+finalfight.leftOffset = 16;
+finalfight.screenWidth = 68;
+finalfight.screenHeight = 53;
+finalfight.image = 'final fight.png';
+bezelDimensions['final_fight'] = finalfight;
+
+var goldenaxe = new Bezel();
+goldenaxe.id = 'golden axe';
+goldenaxe.topOffset = 22;
+goldenaxe.leftOffset = 16;
+goldenaxe.screenWidth = 68;
+goldenaxe.screenHeight = 56;
+goldenaxe.image = 'golden axe.png';
+bezelDimensions['golden_axe'] = goldenaxe;
+
+var kungfumaster = new Bezel();
+kungfumaster.id = 'kung-fu master';
+kungfumaster.topOffset = 20;
+kungfumaster.leftOffset = 15;
+kungfumaster.screenWidth = 70;
+kungfumaster.screenHeight = 60;
+kungfumaster.image = 'kung-fu master.png';
+bezelDimensions['kung-fu_master'] = kungfumaster;
+
+var shinobi = new Bezel();
+shinobi.id = 'shinobi';
+shinobi.topOffset = 23;
+shinobi.leftOffset = 17;
+shinobi.screenWidth = 67;
+shinobi.screenHeight = 54;
+shinobi.image = 'shinobi.png';
+bezelDimensions['shinobi'] = shinobi;
+
+//Fighting
+
+var karatechamp = new Bezel();
+karatechamp.id = 'karatechamp';
+karatechamp.topOffset = 13;
+karatechamp.leftOffset = 24;
+karatechamp.screenWidth = 52;
+karatechamp.screenHeight = 66;
+karatechamp.image = 'karate champ.png';
+bezelDimensions['karate_champ'] = karatechamp;
+
+var streetfighter = new Bezel();
+streetfighter.id = 'street fighter';
+streetfighter.topOffset = 20;
+streetfighter.leftOffset = 20;
+streetfighter.screenWidth = 60;
+streetfighter.screenHeight = 60;
+streetfighter.image = 'street fighter.png';
+bezelDimensions['street_fighter'] = streetfighter;
+
+var streetfighter2 = new Bezel();
+streetfighter2.id = 'street fighter 2';
+streetfighter2.topOffset = 20;
+streetfighter2.leftOffset = 20;
+streetfighter2.screenWidth = 60;
+streetfighter2.screenHeight = 60;
+streetfighter2.image = 'street fighter 2.png';
+bezelDimensions['street_fighter_2'] = streetfighter;
+
+//guns
+
+var crossbow = new Bezel();
+crossbow.id = 'crossbow';
+crossbow.topOffset = 10;
+crossbow.leftOffset = 10;
+crossbow.screenWidth = 80;
+crossbow.screenHeight = 69;
+crossbow.image = 'crossbow.png';
+bezelDimensions['crossbow'] = crossbow;
+
+var operationwolf = new Bezel();
+operationwolf.id = 'operation wolf';
+operationwolf.topOffset = 7;
+operationwolf.leftOffset = 8;
+operationwolf.screenWidth = 85;
+operationwolf.screenHeight = 67;
+operationwolf.image = 'operation wolf.png';
+bezelDimensions['operation_wolf'] = operationwolf;
+
+//hack and slash
+
+var rastansaga = new Bezel();
+rastansaga.id = 'rastan saga';
+rastansaga.topOffset = 20;
+rastansaga.leftOffset = 15;
+rastansaga.screenWidth = 70;
+rastansaga.screenHeight = 72;
+rastansaga.image = 'rastan saga.png';
+bezelDimensions['rastan_saga'] = rastansaga;
+
 //MAZE
 
 var amidarBezel = new Bezel();
@@ -24,6 +139,24 @@ digdug.leftOffset = 24;
 digdug.image = 'dig dug.png';
 bezelDimensions['dig_dug'] = digdug;
 
+var ladybug = new Bezel();
+ladybug.id = 'ladybug';
+ladybug.topOffset = 7;
+ladybug.leftOffset = 15;
+ladybug.screenWidth = 70;
+ladybug.screenHeight = 80;
+ladybug.image = 'ladybug.png';
+bezelDimensions['ladybug'] = ladybug;
+
+var mrdo = new Bezel();
+mrdo.id = 'mr do!';
+mrdo.topOffset = 8;
+mrdo.leftOffset = 5;
+mrdo.screenWidth = 90;
+mrdo.screenHeight = 82;
+mrdo.image = 'mr do!.png';
+bezelDimensions['mr_do!'] = mrdo;
+
 var mspacman = new Bezel();
 mspacman.id = 'ms pac-man';
 mspacman.topOffset = 20;
@@ -42,7 +175,7 @@ pacman.screenHeight = 70;
 pacman.image = 'pac-man.png';
 bezelDimensions['pac-man'] = pacman;
 
-var pacmania = new Bezel(); //????????
+var pacmania = new Bezel();
 pacmania.id = 'pac-mania';
 pacmania.topOffset = 20;
 pacmania.leftOffset = 20;
@@ -50,6 +183,15 @@ pacmania.screenWidth = 63;
 pacmania.screenHeight = 60;
 pacmania.image = 'pac-mania.png';
 bezelDimensions['pac-mania'] = pacmania;
+
+var rallyx = new Bezel();
+rallyx.id = 'rally x';
+rallyx.topOffset = 20;
+rallyx.leftOffset = 15;
+rallyx.screenWidth = 70;
+rallyx.screenHeight = 65;
+rallyx.image = 'rally x.png';
+bezelDimensions['rally_x'] = rallyx;
 
 var superpacman = new Bezel();
 superpacman.id = 'super pac-man';
@@ -60,7 +202,37 @@ superpacman.screenHeight = 67;
 superpacman.image = 'super pac-man.png';
 bezelDimensions['super_pac-man'] = superpacman;
 
+//misc
+
+var qix = new Bezel();
+qix.id = 'qix';
+qix.topOffset = 15;
+qix.leftOffset = 20;
+qix.screenWidth = 60;
+qix.screenHeight = 80;
+qix.image = 'qix.png';
+bezelDimensions['qix'] = qix;
+
+var tapper = new Bezel();
+tapper.id = 'tapper';
+tapper.topOffset = 19;
+tapper.leftOffset = 13;
+tapper.screenWidth = 75;
+tapper.screenHeight = 65;
+tapper.image = 'tapper.png';
+bezelDimensions['tapper'] = tapper;
+
+
 //PLATFORM
+
+var bombjack = new Bezel();
+bombjack.id = 'bomb jack';
+bombjack.topOffset = 25;
+bombjack.leftOffset = 18;
+bombjack.screenWidth = 64;
+bombjack.screenHeight = 58;
+bombjack.image = 'bomb jack.png';
+bezelDimensions['bomb_jack'] = bombjack;
 
 var bubble_bobbleBezel = new Bezel();
 bubble_bobbleBezel.id = 'bubble bobble';
@@ -69,7 +241,6 @@ bubble_bobbleBezel.leftOffset = 22;
 bubble_bobbleBezel.screenWidth = 55;
 bubble_bobbleBezel.screenHeight = 55;
 bubble_bobbleBezel.image = 'bubble bobble.png';
-
 bezelDimensions['bubble_bobble'] = bubble_bobbleBezel;
 
 var burgertime = new Bezel();
@@ -117,6 +288,15 @@ donkeykong.screenHeight = 63;
 donkeykong.image = 'donkey kong.png';
 bezelDimensions['donkey_kong'] = donkeykong;
 
+var flicky = new Bezel();
+flicky.id = 'flicky';
+flicky.topOffset = 18;
+flicky.leftOffset = 15;
+flicky.screenWidth = 70;
+flicky.screenHeight = 65;
+flicky.image = 'flicky.png';
+bezelDimensions['flicky'] = flicky;
+
 var foodfight = new Bezel(); // WRONG GENRE???
 foodfight.id = 'food fight';
 foodfight.topOffset = 28;
@@ -143,6 +323,15 @@ joust2.screenWidth = 48;
 joust2.screenHeight = 70;
 joust2.image = 'joust 2.png';
 bezelDimensions['joust_2'] = joust2;
+
+var ghoulsnghosts = new Bezel();
+ghoulsnghosts.id = 'ghouls n ghosts';
+ghoulsnghosts.topOffset = 21;
+ghoulsnghosts.leftOffset = 16;
+ghoulsnghosts.screenWidth = 69;
+ghoulsnghosts.screenHeight = 58;
+ghoulsnghosts.image = 'ghouls n ghosts.png';
+bezelDimensions['ghouls_n_ghosts'] = ghoulsnghosts;
 
 var joust = new Bezel();
 joust.id = 'joust';
@@ -180,49 +369,43 @@ popeye.screenHeight = 55;
 popeye.image = 'popeye.png';
 bezelDimensions['popeye'] = popeye;
 
-//Fighting
+var quartet = new Bezel();
+quartet.id = 'quartet';
+quartet.topOffset = 13;
+quartet.leftOffset = 9;
+quartet.screenWidth = 82;
+quartet.screenHeight = 58;
+quartet.image = 'quartet.png';
+bezelDimensions['quartet'] = quartet;
 
-var karatechamp = new Bezel();
-karatechamp.id = 'karatechamp';
-karatechamp.topOffset = 13;
-karatechamp.leftOffset = 24;
-karatechamp.screenWidth = 52;
-karatechamp.screenHeight = 66;
-karatechamp.image = 'karate champ.png';
-bezelDimensions['karate_champ'] = karatechamp;
-
-//guns
-
-var crossbow = new Bezel();
-crossbow.id = 'crossbow';
-crossbow.topOffset = 10;
-crossbow.leftOffset = 10;
-crossbow.screenWidth = 80;
-crossbow.screenHeight = 69;
-crossbow.image = 'crossbow.png';
-bezelDimensions['crossbow'] = crossbow;
-
-//misc
-
-var qix = new Bezel();
-qix.id = 'qix';
-qix.topOffset = 15;
-qix.leftOffset = 20;
-qix.screenWidth = 60;
-qix.screenHeight = 80;
-qix.image = 'qix.png';
-bezelDimensions['qix'] = qix;
-
-var tapper = new Bezel();
-tapper.id = 'tapper';
-tapper.topOffset = 19;
-tapper.leftOffset = 13;
-tapper.screenWidth = 75;
-tapper.screenHeight = 65;
-tapper.image = 'tapper.png';
-bezelDimensions['tapper'] = tapper;
+var strider = new Bezel();
+strider.id = 'strider';
+strider.topOffset = 15;
+strider.leftOffset = 17;
+strider.screenWidth = 68;
+strider.screenHeight = 70;
+strider.image = 'strider.png';
+bezelDimensions['strider'] = strider;
 
 //Puzzle
+
+var bubbles = new Bezel();
+bubbles.id = 'bubbles';
+bubbles.topOffset = 5;
+bubbles.leftOffset = 15;
+bubbles.screenWidth = 73;
+bubbles.screenHeight = 81;
+bubbles.image = 'bubbles.png';
+bezelDimensions['bubbles'] = bubbles;
+
+var klax = new Bezel();
+klax.id = 'klax';
+klax.topOffset = 22;
+klax.leftOffset = 16;
+klax.screenWidth = 70;
+klax.screenHeight = 56;
+klax.image = 'klax.png';
+bezelDimensions['klax'] = klax;
 
 var qbert = new Bezel();
 qbert.id = 'q-bert';
@@ -234,6 +417,42 @@ qbert.image = 'q-bert.png';
 bezelDimensions['q-bert'] = qbert;
 
 //racing
+
+var afterburner = new Bezel();
+afterburner.id = 'after burner';
+afterburner.topOffset = 30;
+afterburner.leftOffset = 20;
+afterburner.screenWidth = 60;
+afterburner.screenHeight = 45;
+afterburner.image = 'after burner.png';
+bezelDimensions['after_burner'] = afterburner;
+
+var allpointsbulletin = new Bezel();
+allpointsbulletin.id = 'all points bulletin';
+allpointsbulletin.topOffset = 17;
+allpointsbulletin.leftOffset = 15;
+allpointsbulletin.screenWidth = 70;
+allpointsbulletin.screenHeight = 67;
+allpointsbulletin.image = 'all points bulletin.png';
+bezelDimensions['all_points_bulletin'] = allpointsbulletin;
+
+var badlands = new Bezel();
+badlands.id = 'badlands';
+badlands.topOffset = 12;
+badlands.leftOffset = 15;
+badlands.screenWidth = 70;
+badlands.screenHeight = 75;
+badlands.image = 'badlands.png';
+bezelDimensions['badlands'] = badlands;
+
+var buggyboy = new Bezel();
+buggyboy.id = 'buggy boy';
+buggyboy.topOffset = 22;
+buggyboy.leftOffset = 17;
+buggyboy.screenWidth = 65;
+buggyboy.screenHeight = 60;
+buggyboy.image = 'buggy boy.png';
+bezelDimensions['buggy_boy'] = buggyboy;
 
 var chasehq = new Bezel();
 chasehq.id = 'chase h q';
@@ -253,7 +472,107 @@ superbug.screenHeight = 70;
 superbug.image = 'super bug.png';
 bezelDimensions['super_bug'] = superbug;
 
+var superhangon = new Bezel();
+superhangon.id = 'super hang on';
+superhangon.topOffset = 22;
+superhangon.leftOffset = 24;
+superhangon.screenWidth = 52;
+superhangon.screenHeight = 75;
+superhangon.image = 'super hang on.png';
+bezelDimensions['super_hang_on'] = superhangon;
+
+//Run and gun
+
+var commando = new Bezel();
+commando.id = 'commando';
+commando.topOffset = 16;
+commando.leftOffset = 20;
+commando.screenWidth = 60;
+commando.screenHeight = 69;
+commando.image = 'commando.png';
+bezelDimensions['commando'] = commando;
+
+var ikariwarriors = new Bezel();
+ikariwarriors.id = 'ikari warriors';
+ikariwarriors.topOffset = 14;
+ikariwarriors.leftOffset = 17;
+ikariwarriors.screenWidth = 67;
+ikariwarriors.screenHeight = 70;
+ikariwarriors.image = 'ikari warriors.png';
+bezelDimensions['ikari_warriors'] = ikariwarriors;
+
+var mercs = new Bezel();
+mercs.id = 'mercs';
+mercs.topOffset = 10;
+mercs.leftOffset = 20;
+mercs.screenWidth = 60;
+mercs.screenHeight = 80;
+mercs.image = 'mercs.png';
+bezelDimensions['mercs'] = mercs;
+
 //Shooter
+var _1942 = new Bezel();
+_1942.id = '1942';
+_1942.topOffset = 15;
+_1942.leftOffset = 23;
+_1942.screenWidth = 52;
+_1942.screenHeight = 64;
+_1942.image = '1942.png';
+bezelDimensions['1942'] = _1942;
+
+var _1943 = new Bezel();
+_1943.id = '1943';
+_1943.topOffset = 16;
+_1943.leftOffset = 23;
+_1943.screenWidth = 54;
+_1943.screenHeight = 68;
+_1943.image = '1943.png';
+bezelDimensions['1943'] = _1943;
+
+var alcon = new Bezel();
+alcon.id = 'alcon';
+alcon.topOffset = 17;
+alcon.leftOffset = 24;
+alcon.screenWidth = 52;
+alcon.screenHeight = 68;
+alcon.image = 'alcon.png';
+bezelDimensions['alcon'] = alcon;
+
+var assteroidsdeluxe = new Bezel();
+assteroidsdeluxe.id = 'asteroids deluxe';
+assteroidsdeluxe.topOffset = 0;
+assteroidsdeluxe.leftOffset = 10;
+assteroidsdeluxe.screenWidth = 80;
+assteroidsdeluxe.screenHeight = 80;
+assteroidsdeluxe.image = 'asteroids deluxe.png';
+bezelDimensions['asteroids_deluxe'] = assteroidsdeluxe;
+
+var asteroids = new Bezel();
+asteroids.id = 'asteroids';
+asteroids.topOffset = 10;
+asteroids.leftOffset = 10;
+asteroids.screenWidth = 80;
+asteroids.screenHeight = 80;
+asteroids.image = 'asteroids.png';
+bezelDimensions['asteroids'] = asteroids;
+
+var berzerk = new Bezel();
+berzerk.id = 'berzerk';
+berzerk.topOffset = 17;
+berzerk.leftOffset = 13;
+berzerk.screenWidth = 74;
+berzerk.screenHeight = 67;
+berzerk.image = 'berzerk.png';
+bezelDimensions['berzerk'] = berzerk;
+
+var carnival = new Bezel();
+carnival.id = 'carnival';
+carnival.topOffset = 33;
+carnival.leftOffset = 20;
+carnival.screenWidth = 60;
+carnival.screenHeight = 64;
+carnival.image = 'carnival.png';
+bezelDimensions['carnival'] = carnival;
 
 var centipede = new Bezel();
 centipede.id = 'centipede';
@@ -291,6 +610,15 @@ galaxian.screenHeight = 70;
 galaxian.image = 'galaxian.png';
 bezelDimensions['galaxian'] = galaxian;
 
+var gyruss = new Bezel();
+gyruss.id = 'gyruss';
+gyruss.topOffset = 13;
+gyruss.leftOffset = 25;
+gyruss.screenWidth = 50;
+gyruss.screenHeight = 80;
+gyruss.image = 'gyruss.png';
+bezelDimensions['gyruss'] = gyruss;
+
 var junofirst = new Bezel();
 junofirst.id = 'juno first';
 junofirst.topOffset = 13;
@@ -318,6 +646,24 @@ robotron.screenHeight = 65;
 robotron.image = 'robotron 2084.png';
 bezelDimensions['robotron_2084'] = robotron;
 
+var scramble = new Bezel();
+scramble.id = 'scramble';
+scramble.topOffset = 20;
+scramble.leftOffset = 24;
+scramble.screenWidth = 53;
+scramble.screenHeight = 74;
+scramble.image = 'scramble.png';
+bezelDimensions['scramble'] = scramble;
+
+var spaceinvaders = new Bezel();
+spaceinvaders.id = 'space invaders';
+spaceinvaders.topOffset = 30;
+spaceinvaders.leftOffset = 20;
+spaceinvaders.screenWidth = 60;
+spaceinvaders.screenHeight = 50;
+spaceinvaders.image = 'space invaders.png';
+bezelDimensions['space_invaders'] = spaceinvaders;
+
 var sinistar = new Bezel();
 sinistar.id = 'sinistar';
 sinistar.topOffset = 18;
@@ -336,6 +682,15 @@ spacezap.screenHeight = 75;
 spacezap.image = 'space zap.png';
 bezelDimensions['space_zap'] = spacezap;
 
+var therealghostbusters = new Bezel();
+therealghostbusters.id = 'the real ghostbusters';
+therealghostbusters.topOffset = 24;
+therealghostbusters.leftOffset = 15;
+therealghostbusters.screenWidth = 70;
+therealghostbusters.screenHeight = 52;
+therealghostbusters.image = 'the real ghostbusters.png';
+bezelDimensions['the_real_ghostbusters'] = therealghostbusters;
+
 var tron = new Bezel();
 tron.id = 'tron';
 tron.topOffset = 15;
@@ -353,6 +708,15 @@ warlords.screenWidth = 84;
 warlords.screenHeight = 65;
 warlords.image = 'warlords.png';
 bezelDimensions['warlords'] = warlords;
+
+var xenophobe = new Bezel();
+xenophobe.id = 'xenophobe';
+xenophobe.topOffset = 5;
+xenophobe.leftOffset = 15;
+xenophobe.screenWidth = 70;
+xenophobe.screenHeight = 90;
+xenophobe.image = 'xenophobe.png';
+bezelDimensions['xenophobe'] = xenophobe;
 
 function GetBezel(gamename) {
 
